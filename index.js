2e22ffa143e2c670c17b9488a41f31cbb8a4fda1
Confused <48794324+ConfusedCoflicted@users.noreply.github.com>
2019-05-04 15:34:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const client = require('discord-rich-presence')('574094643894157313');
-const ascii = require('ascii-art')
-const readline = require('readline')
+const ascii = require('ascii-art');
+const readline = require('readline');
+const package = require('./package.json')
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,7 +11,7 @@ const rl = readline.createInterface({
 function ask() {
 ascii.font('Custom-RPC', 'Doom', function(rendered) {
 console.log(rendered)
-rl.question('Presence\'s Details? EX. Playing A Game, Learning something\nMade By Conflicted.\n> ', (answer) => {
+rl.question('Version :'+ package.version +'\nPresence\'s Details? EX. Playing A Game, Learning something\nMade By Conflicted.\n> ', (answer) => {
 if(!answer) { console.log('Please And Please! Specify Something!'); ask()}
 if(answer.length < 2) { console.log('Please specify something that are 2 charector long!'); ask()}
 if(answer.toLowerCase() === 'quit' || answer.toLowerCase() === 'left' || answer.toLowerCase() === 'exit') { console.clear; console.log('Exited The Process, The RPC will take sometime to update.'); rl.close(); return; process.exit;}
