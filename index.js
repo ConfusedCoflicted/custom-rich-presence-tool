ff85d5aba8e5497ea13dc82fd389f2e318d5bacb
Confused <48794324+ConfusedCoflicted@users.noreply.github.com>
2019-05-04 14:10:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ if(!answer) { console.log('Please And Please! Specify Something!'); ask()}
 if(answer.length < 2) { console.log('Please specify something that are 2 charector long!'); ask()}
 if(answer.toLowerCase() === 'quit' || answer.toLowerCase() === 'left' || answer.toLowerCase() === 'exit') { console.clear; console.log('Exited The Process, The RPC will take sometime to update.'); rl.close(); return; process.exit;}
 client.updatePresence({
-  details: answer,
-  state: "By Conflicted",
+  state: answer,
+  details: "Made By Conflicted",
   startTimestamp: Date.now(),
   largeImageKey: "profile",
   largeImageText: "Made By Conflicted",
