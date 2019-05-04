0cf3299ad0bbd375fd4d6baf443bd90c932af918
Confused <48794324+ConfusedCoflicted@users.noreply.github.com>
2019-05-04 13:20:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const rl = readline.createInterface({
 function ask() {
 
 rl.question('Presence\'s Details? EX. Playing A Game, Learning something\nMade By Conflicted.\n> ', (answer) => {
-if(!answer) return console.log('Please And Please! Specify Something!')
-if(answer.length < 2) return console.log('Please specify something that are 2 charector long!')
+if(!answer) { console.log('Please And Please! Specify Something!'); ask()}
+if(answer.length < 2) { console.log('Please specify something that are 2 charector long!'); ask()}
+if(answer.toLowerCase() === 'quit' || answer.toLowerCase() === 'left' || answer.toLowerCase() === 'exit') { console.clear; console.log('Exited The Process, The RPC will take sometime to update.'); rl.close(); return; process.exit;}
 client.updatePresence({
   details: answer,
   state: "Custom Rich Presence",
+  startTimestamp: Date.now(),
   largeImageKey: "profile",
   largeImageText: "Made By Conflicted",
   smallImageKey: 'smallimage',
